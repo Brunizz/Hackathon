bdbbb137c1fa39acf52f6ece35d34ff736d6686d
Marcus Brunila <brunila.marcus@gmail.com>
2022-04-20 01:00:15 +0300

    added CAT

--- cat/main.go
+++ cat/main.go
@@ -11,14 +11,32 @@ func main() {
 	if len(args) < 1 {
 		filecontent, error := ioutil.ReadFile(os.Stdin.Name())
 		if error != nil {
-			Print("ERROR: open "+args[0]+": no such file or directory")
+			Print("ERROR: open "+args[0]": no such file or directory")
 			os.exit(1)
 		} else {
 			Print(string(filecontent))
 		}
 	} else {
 		for i := 0, i < len(args); i++ {
-			filecontent, error :=
+			filecontent, error := ioutil.ReadFile(args[i])
+			if error != nil {
+				Printagain("ERROR: open "+args[i]": no such file or directory")
+				os.exit(1)
+			} else [
+				Print(string(filecontent))
+			]
 		}
+	}
+}
+
+func Print(a string) {
+	for _, printstring := range a {
+		z01.PrintRune(printstring)
+	}
+	z01.PrintRune(\'n')
+}
+func Printagain(a string) {
+	for _, printstring := range a {
+		z01.PrintRune(printstring)
 	}
 }
