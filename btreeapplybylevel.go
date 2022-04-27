28eaa37fd5359421f050ee26a64607f7dfb2e3cc
Brunizz <102155727+Brunizz@users.noreply.github.com>
2022-04-27 14:56:55 +0300

    Add files via upload

---
+++ btreeapplybylevel.go
@@ -0,0 +1,20 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	height := BTreeLevelCount(root)
+	for i := 0; i < height; i++ {
+		applylevel(root, i, f)
+	}
+}
+
+func applylevel(root *TreeNode, level int, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+	if level == 0 {
+		f(root.Data)
+	} else if level > 0 {
+		applylevel(root.Left, level-1, f)
+		applylevel(root.Right, level-1, f)
+	}
+}
